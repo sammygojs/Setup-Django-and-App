5f59f7bb33b23bd18c8dff641f586b7c855f0334
Sumit <akoliyasumit@hotmail.com>
2023-01-12 20:19:33 +0530

    trying class based views in demoapp

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('class', views.MyView.as_view())
 ]
--- demoapp/views.py
+++ demoapp/views.py
@@ -2,3 +2,14 @@ from django.http import HttpResponse
 
 def index(request): 
     return HttpResponse("Hello, world. This is the index view of Demoapp.") 
+
+from django.views import View 
+
+class MyView(View): 
+    def get(self, request): 
+        # logic to process GET request
+        return HttpResponse('response to GET request') 
+ 
+    def post(self, request): 
+        # <logic to process POST request> 
+        return HttpResponse('response to POST request') 
